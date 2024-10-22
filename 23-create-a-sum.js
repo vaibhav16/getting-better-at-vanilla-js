dd0ef0ad343ee8faf932ffde0fe48c732e7b98e7
Vaibhav Sharma <vssharma.vaibhav5@gmail.com>
2024-10-23 00:46:19 +0530

    create a sum

---
+++ 23-create-a-sum.js
@@ -0,0 +1,6 @@
+// Create a sum(), which makes following possible
+// const sum1 = sum(1)
+// sum1(2) == 3 // true
+// sum1(3) == 4 // true
+// sum(1)(2)(3) == 6 // true
+// sum(5)(-1)(2) == 6 // true
