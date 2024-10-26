4d5aeba35263997525a64c885884a69752a5d072
Vaibhav Sharma <vssharma.vaibhav5@gmail.com>
2024-10-26 19:21:37 +0530

    intersection using set

---
+++ 167-Intersection-of-unsorted-arrays.js
@@ -0,0 +1,18 @@
+// 167. Intersection of unsorted arrays
+// JavaScript
+// Algorithm
+// met this in an interview?
+//  33
+// Add to my list
+// Share
+
+// easy  5381 accepted / 14047 tried
+
+//  Well done!You have solved it!unsubmit
+
+// Given two arrays, find the intersection(items occur in both arrays)
+
+// arrays are not sorted, and might have duplicates.
+// you can modify the arrays
+// you can return the items in any order, but without duplicates.
+// This is an easy problem, What is the time & space complexity of your approach?
